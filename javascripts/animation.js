6da8e1ef680921986a6e7d66d3af35406bfeddce
russkaya-kartoshka <ogavelaoi@gmail.com>
2017-03-02 04:50:29 +0300

    yas!

--- javascripts/animation.js
+++ javascripts/animation.js
@@ -1,3 +1,21 @@
-$("#cassette").click(function startAnimation('dance') {
-  $('.cassette').toggleClass('dance');
+$(function(){
+	$('.cassette').click(function(){
+		e1 = $('.cassette');
+        e1.addClass('animate');
+				e1.one('webkitAnimationEnd oanimationend msAnimationEnd animationend',
+        function (e) {
+            e1.removeClass('animate');
+        });
+	});
+});
+
+$(function(){
+	$('.cassette').click(function(){
+		e1 = $('body');
+        e1.addClass('animate1');
+        e1.one('webkitAnimationEnd oanimationend msAnimationEnd animationend',
+        function (e) {
+            e1.removeClass('animate1');
+        });
+	});
 });
