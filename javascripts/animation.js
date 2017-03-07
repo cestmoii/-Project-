c88696bd7d3df12b510157270be2af930fa2a92e
russkaya-kartoshka <ogavelaoi@gmail.com>
2017-03-07 03:49:41 +0300

    no

--- javascripts/animation.js
+++ javascripts/animation.js
@@ -9,3 +9,17 @@ $(function(){
 	  $('.cassette').toggleClass('animate');
 	});
 });
+
+$(function(){
+	e1 = $('body');
+	e1.addClass('animate2');
+	$('.cassette').click(function(){
+		e1.removeClass('animate2');
+	});
+});
+
+$(function(){
+	$('.cassette').click(function(){
+		$('.figs').addClass('animate3');
+	});
+});
